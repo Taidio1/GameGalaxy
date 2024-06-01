cf2b692d49012f0c1669a2fa30652fccc616a101
Kapi <zuzannaraszka@gmail.com>
2024-06-01 19:50:54 +0200

    Dodaj pliki projektów.

---
+++ GameGalaxy/Entities/GameShopEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace GameGalaxy.Entities
+{
+    public class GameShopEntity
+    {
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public string Address { get; set; } = default!;
+    }
+}
