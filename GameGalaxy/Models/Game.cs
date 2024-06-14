ac56d5a6652323d020fb31d39e737d210ccdfdd4
Igor <115781273+Igdud@users.noreply.github.com>
2024-06-14 13:10:06 +0200

    zmiana 2 parametrow modelu na required

--- GameGalaxy/Models/Game.cs
+++ GameGalaxy/Models/Game.cs
@@ -2,11 +2,11 @@
 {
     public class Game
     {
-        public string ImageUrl { get; set; }
+        public required string ImageUrl { get; set; }
         public int Id { get; set; }
         public string? Name { get; set; }
         public double Rating { get; set; }
-        public string Producer { get; set; }
+        public required string Producer { get; set; }
         public decimal Price { get; set; }
         
         public int ReleaseDate { get; set; }
